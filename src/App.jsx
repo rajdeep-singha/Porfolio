f4f9c93dfd565952e38286bf7a6d66af1cdb8906
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2024-06-14 21:44:14 +0530

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import Navbar from "./components/navbar/Navbar.jsx";
+import './App.scss';
+
+const App =() =>{
+
+
+  return 
+    
+      <div>
+        <section>
+         <Navbar/>
+        </section>
+        <section>Parallax</section>
+        <section>Services</section>
+        <section>Parallax</section>
+        <section>Portfolio1</section>
+        <section>Portfolio2</section>
+        <section>Portfolio3</section>
+        <section>Contact</section>
+      </div>
+    
+  
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.scss"
+
+ const Navbar = () => {
+  return (
+    <div className="navbar">
+    {/* Sidebar */}
+    <div className="wrapper">
+        <span>Rajdeep</span>
+      <div className="social">
+        <a href="#"><img src="Portfolio/public/facebook.png" alt=""/></a>
+      </div>
+    </div>
+    </div>
+  )
+}
+export default Navbar

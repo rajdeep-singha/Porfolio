aa07feeb2e30b4bb6f5621e950f92f0fe63896eb
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2024-06-26 16:53:21 +0530

    parallax functioning

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Test from "./Test";
 import "./App.scss";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
-
+import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 const App = () => {
   return (
     <>
@@ -10,9 +11,9 @@ const App = () => {
         <section id="Homepage">
           <Hero />
         </section>
-        <section id="Services">Parallax</section>
-        <section>Services</section>
-        <section id="Portfolio">Parallax</section>
+        <section id="Services"><Parallax type="services" /></section>
+        <section><Services/></section>
+        <section id="Portfolio"><Parallax type="portfolio"/></section>
         <section>Portfolio1</section>
         <section>Portfolio2</section>
         <section>Portfolio3</section>

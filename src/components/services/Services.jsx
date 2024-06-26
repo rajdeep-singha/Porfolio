e7a1b5a33d08f3cb1f493a35c36ab8dbc56b12e8
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2024-06-26 21:35:57 +0530

    services updated

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,47 +1,78 @@
 import "./services.scss"
-import {motion} from "framer-motion"
+import { useRef } from "react";
+import {animate, motion,useInView} from "framer-motion"
 
+const variants={
+    initial:{
+        x:-500,
+        y:100,
+        opacity:0,
+    },
+    animate:{
+        x:0,
+        opacity:1,
+        y:0,
+        transition:{
+          duration:1,
+           staggeredChildren:0.1,
+        },
+    },
+};
  const Services = () => {
+    const ref= useRef()
+    
+    const isInView = useInView(ref,{margin:"-100px"})
   return (
-    <motion.div className="Services">
-        <motion.div className="textContainer">
+    <motion.div 
+    className="Services" 
+    variants={variants}
+    initial="initial"
+    // animate="animate"
+    //whileInView="animate"
+    ref={ref}
+    animate={isInView&& "animate"}
+    >
+        <motion.div className="textContainer" 
+         variants={variants}>
             <p>I focus on helping your brand grow 
                 <br />and move forward</p>
             <hr/>
         </motion.div>
-        <motion.div className="titleContainer">
+        <motion.div className="titleContainer"
+         variants={variants}>
            <div className="title">
           <img src="/people.webp"></img>
           <h1>
-            <b>Unique</b>Ideas
+            <b>Unique</b> Ideas
             </h1>
             </div> 
 
            <div className="title">
-          <h1><b>For Your</b>Business</h1>
+          <h1><b>For Your</b> Business</h1>
           <button>WHAT WE DO?</button>
             </div> 
         </motion.div>
-        <motion.div className="listContainer">
-            <div className="box">
+        <motion.div className="listContainer" 
+         variants={variants}>
+            <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
                 <h2>Branding</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam id iusto rerum, perferendis impedit corrupti assumenda rem ratione ipsam sunt nisi! Iure voluptatem hic voluptate! Porro necessitatibus et commodi officiis.</p>
-            </div>
+            </motion.div>
             <button>GO</button>
-            <div className="box">
+            <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
                 <h2>Branding</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam id iusto rerum, perferendis impedit corrupti assumenda rem ratione ipsam sunt nisi! Iure voluptatem hic voluptate! Porro necessitatibus et commodi officiis.</p>
-            </div>
+            </motion.div>
             <button>GO</button>
-            <div className="box">
+            <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
                 <h2>Branding</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam id iusto rerum, perferendis impedit corrupti assumenda rem ratione ipsam sunt nisi! Iure voluptatem hic voluptate! Porro necessitatibus et commodi officiis.</p>
-            </div>
+            </motion.div>
             <button>GO</button>
-            <div className="box">
+            <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
                 <h2>Branding</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam id iusto rerum, perferendis impedit corrupti assumenda rem ratione ipsam sunt nisi! Iure voluptatem hic voluptate! Porro necessitatibus et commodi officiis.</p>
-            </div>
+            </motion.div>
             <button>GO</button>
         </motion.div>
     </motion.div>
